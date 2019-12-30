57aaab8d3a0fd6a2e3b5160d1bc12ad4c38ead87
Jack Hayhurst <jhayhurst@liquidweb.com>
2019-12-30 11:45:24 -0500

    added a bash snippet to parse cli flags
    Signed-off-by: Jack Hayhurst <jhayhurst@liquidweb.com>

---
+++ parseFlags.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#! source this to parse the flags and leave positional args in place
+PARAMS=""
+while (( "$#" )); do
+  case "$1" in
+    -f|--flag-with-argument)
+      FARG=$2
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
